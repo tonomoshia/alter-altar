c9d1c41db85569e5dffaca5c78d0c739702c5227
Elizabeth Reiher <tonomoshia@gmail.com>
2017-03-21 00:41:59 -0400

    automatic sprites with gulp (2)

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,25 +1,35 @@
-var gulp = require ('gulp'),
-svgSprite = require ('gulp-svg-sprite');
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
-    mode: {
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
         css: {
-            render: {
-                css: {
-                    template: './gulp/templates/sprite.css'
-                }
-            }
+          template: './gulp/templates/sprite.css'
         }
+      }
     }
+  }
 }
 
 gulp.task('createSprite', function() {
-    return gulp.src('./app/assets/images/icons/**/*.svg')
+  return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteCSS', function() {
-    return gulp.src("./app/temp/sprite/css/*.css")
-        .pipe(gulp.dest('./app/assets/styles/modules'));
-    });
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
