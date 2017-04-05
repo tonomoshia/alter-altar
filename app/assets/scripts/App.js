29ba239397a937ba6fc6f45ca7c6b9f14b0103ec
Elizabeth Reiher <tonomoshia@gmail.com>
2017-04-05 18:22:14 -0400

    add jquery for mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,3 @@
-var $ = require('jquery');
-var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-alert("ABC 321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Person("Jane Smith", "green");
-jane.greet();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
