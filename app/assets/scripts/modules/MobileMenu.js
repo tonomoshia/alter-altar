f38f379505848e88a168966f2b7a3a0947ef3639
Elizabeth Reiher <tonomoshia@gmail.com>
2017-04-10 13:56:13 -0400

    modifying mobile menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,17 +2,19 @@ import $ from 'jquery';
 
 class MobileMenu {
    constructor () {
+       this.siteHeader = $(".site-header");
        this.menuIcon = $(".site-header__menu-icon");
        this.menuContent = $(".site-header__menu-content");
        this.events();
-   } 
-    
+   }
+
     events() {
         this.menuIcon.click(this.toggleTheMenu.bind(this));
     }
-    
+
     toggleTheMenu() {
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
+        this.siteHeader.toggleClass("site-header--is-expanded");
     }
 }
 
